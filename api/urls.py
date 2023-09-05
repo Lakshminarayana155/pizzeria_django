3ed0a23c11963d4a88857a89b5962c863c37c82d
LakshmiNarayana <lakshminarayanamanchuri@gmail.com>
2023-09-05 16:29:36 +0530

    added a feature to get the pizzaorder using id

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import PizzaOrderAPIView
 
 urlpatterns = [
-    path('orderpizza/',PizzaOrderAPIView.as_view(), name="orderpizza")
+    path('orderpizza/',PizzaOrderAPIView.as_view(), name="orderpizza"),
+    path('pizzaorders/<int:id>/', PizzaOrderAPIView.as_view(), name="pizzaorder-detail"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,30 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import PizzaOrder
+from .models import PizzaOrder,Topping
 from .serializers import PizzaOrderSerializer
 
 class PizzaOrderAPIView(APIView):
+    def get(self, request,id):
+        try:
+            order = PizzaOrder.objects.get(id=id)
+        except PizzaOrder.DoesNotExist:
+            return Response(
+                {"error": "Pizza order not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        serializer = PizzaOrderSerializer(order)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     def post(self, request):
+        existing_toppings_count = Topping.objects.count()
+
+        if existing_toppings_count < 7:
+            # Create the missing toppings
+            required_toppings = ['Pepperoni','Mushrooms','Green Peppers','Onions','Black Olives','Sausage','Pineapple']
+            for topping_name in required_toppings:
+                Topping.objects.get_or_create(name=topping_name)
         order_data_list = request.data  # Assuming you receive a list of orders
 
         # List to store created order instances
@@ -24,10 +43,4 @@ class PizzaOrderAPIView(APIView):
         # Return a 201 response with data for all created orders
         return Response(PizzaOrderSerializer(created_orders, many=True).data, status=status.HTTP_201_CREATED)
     
-    # def post(self, request):
-    #     serializer = PizzaOrderSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         # Valid data, create the PizzaOrder instance
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
